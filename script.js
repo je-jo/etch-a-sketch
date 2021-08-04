7031c6813e64b559acdab1e793061216c850e0c8
Jelena Jovicic <jejovicic@gmail.com>
2021-08-04 20:16:09 +0200

    Add colors and custom grid size

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 const container = document.getElementById("container");
 let size = 16;
-container.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr);`);
+const square = size ** 2;
+const cells = document.getElementsByClassName("cell"); //live collection
 
-const square = size * size;
+createGrid(size);
 
-
-for (i = 1; i <= square; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    container.appendChild(cell);
+function createGrid(size) {
+    clearAll();
+    for (i = 1; i <= size ** 2; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+        container.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr);`);
+    }
+    addListener();
 }
 
-const cells = document.getElementsByClassName("cell");
 
-for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('mouseenter', myFunction);
+
+function addListener() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseenter', colorCell);
+    }
 }
 
-function myFunction(e) {
-    e.target.style.backgroundColor = "cadetblue";
+function colorCell(e) {
+    e.target.style.backgroundColor = `${color}`;
 }
 
 const clear = document.getElementById("clear");
+clear.addEventListener("click", eraser);
 
-clear.addEventListener("click", clearAll)
+function eraser() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].style.backgroundColor = "white"
+    }
+}
 
 function clearAll() {
-    for (var i = 0; i < cells.length; i++) {
-        cells[i].style.backgroundColor = "white";
-    }}
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const colorButtons = document.getElementsByClassName("pallete");
+
+let color = "pink"
+
+//function setColor() {
+    for (let i = 0; i < colorButtons.length; i++) {
+        colorButtons[i].addEventListener('click', function (e) {
+            color = e.currentTarget.id;
+        e.currentTarget.classList.add("active");
+        });
+    } 
+// }
+
+const inputSize = document.getElementById("gridsize");
+inputSize.addEventListener("change", updateValue);
+function updateValue(e) {
+  size = e.target.value;
+}
+
+const createButton = document.getElementById("creategrid");
+createButton.addEventListener("click", function (e) {
+    createGrid(size);
+});
+
+
+
+
