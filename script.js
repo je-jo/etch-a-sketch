10df6c28d672b9720e6694e0977502ac65b45076
Jelena Jovicic <jejovicic@gmail.com>
2021-08-15 00:22:11 +0200

    working opacity and paint method

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const container = document.getElementById("container");
 let size = 16;
 const cells = document.getElementsByClassName("cell"); //live collection
-let paintMethod = "click";
+let paintMethod = "mouseenter";
 let opacity = 1;
-let currentOpacity;
 
 
 
 function createGrid(size) {
     clearAll();
-    for (i = 1; i <= size ** 2; i++) {
+    for (let i = 1; i <= size ** 2; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
         container.appendChild(cell);
@@ -24,9 +23,33 @@ function addListener() {
     }
 }
 
-function colorCell() {
-    this.style.backgroundColor = `${color}`;
-    this.style.opacity = `${opacity}`;
+function removeListener() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener(`${paintMethod}`, colorCell);
+    }
+}
+
+// transparency
+const transparency = document.getElementsByName("opacity");
+for (let i = 0; i < transparency.length; i++) {
+    transparency[i].addEventListener("click", function (e) {
+        opacity = +e.currentTarget.id;
+        console.log("Opacity is set to " + opacity)
+    });
+};
+
+function colorCell(e) {
+    e.currentTarget.style.backgroundColor = `${color}`;
+
+    if (opacity <= 0.9 && e.currentTarget.style.opacity <= 0.9) { //without the first condition, opacity of 1 becomes 0.1
+        opacity = e.currentTarget.style.opacity; // without e.currentTarget line, the counter never resets for single cell
+        e.currentTarget.style.opacity = (+`${opacity}` + 0.1);
+    }
+    else {
+        e.currentTarget.style.opacity = 1;
+    }
+
+    console.log(e.currentTarget.style.opacity)
 }
 
 function clearAll() {
@@ -66,18 +89,14 @@ for (let i = 0; i < colorButtons.length; i++) {
 const methods = document.getElementsByName("method");
 for (let i = 0; i < methods.length; i++) {
     methods[i].addEventListener("click", function (e) {
+        removeListener(); //remove old listener before asigning new value
         paintMethod = e.currentTarget.id;
-        console.log(paintMethod);
+        console.log("Paint method is set to " + paintMethod);
+        addListener()
     });
 };
 
-// transparency
-const transparency = document.getElementsByName("opacity");
-for (let i = 0; i < transparency.length; i++) {
-    transparency[i].addEventListener("click", function (e) {
-        opacity = +e.currentTarget.id;
-    });
-};
+
 
 
 
